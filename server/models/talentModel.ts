7b31c34770ea693faef67de6a3bb1e18d58cf43f
Jojo Thomas <jojothomas1515@gmail.com>
2023-10-07 15:55:19 +0100

    Created talents model and schema and added interface for it.

--- server/models/talentModel.ts
+++ server/models/talentModel.ts
@@ -1,1 +1,62 @@
-import { Model } from "mongoose";
+import { model, Schema } from 'mongoose';
+import {
+    Certification,
+    Education,
+    Experience,
+    Project,
+    Talent,
+} from '../interfaces/talentInterface';
+
+const experienceSchema = new Schema<Experience>(
+    {
+        jobTitle: String,
+        description: String,
+        employer: String,
+        city: String,
+        country: String,
+        startDate: Schema.Types.Date,
+        endDate: Schema.Types.Date,
+    },
+    { _id: false },
+);
+const educationSchema = new Schema<Education>(
+    {
+        degree: String,
+        description: String,
+        city: String,
+        country: String,
+        startDate: Schema.Types.Date,
+        endDate: Schema.Types.Date,
+        school: String,
+    },
+    { _id: false },
+);
+const certificationSchema = new Schema<Certification>(
+    { link: String, title: String },
+    { _id: false },
+);
+const projectSchema = new Schema<Project>(
+    { name: String, description: String, link: String },
+    { _id: false },
+);
+
+const talentSchema = new Schema<Talent>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    hashedPassword: { type: String, required: true },
+    experience: [experienceSchema],
+    skill: [String],
+    education: [educationSchema],
+    bio: String,
+    certifications: [certificationSchema],
+    github: String,
+    socials: [String],
+    hasOffer: { type: Boolean, default: false },
+    profileImg: String,
+    rank: { type: Number, default: 0 },
+    projects: [projectSchema],
+});
+
+const Talent = model<Talent>('talents', talentSchema);
+
+export default Talent;
