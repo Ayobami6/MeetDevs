0826c684e24a074bbf09cf1267fa5b757ab2fb81
Ayobami <ayobamidele006@gmail.com>
2023-10-09 18:29:54 +0100

    feat: implement initial auth page

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import SignIn from './SignIn';
+import SignUp from './Signup';
 
 const Auth: React.FC = () => {
-  return (
-    <>
-    <div>Auth</div>
+    const [isSignup, setIsSignup] = useState(false);
 
-        <div>Auth Page</div>
-    </>
-  )
-    
-}
+    const handleIsMemberClick = () => {
+        setIsSignup(!isSignup);
+    };
 
-export default Auth
+    return (
+        <>
+            <div>
+                {/* <div className="bg-[url('src/assets/auth3.jpg')] bg-img w-full bg-cover">
+                    
+                </div> */}
+                <img
+                    src='src/assets/auth3.jpg'
+                    className='bg-img bg-cover w-fit'
+                />
+                <div className='text-7xl mx-5 py-4 text-green-900'>
+                    MeetDevs
+                </div>
+                {isSignup ? (
+                    <SignUp handleIsMemberClick={handleIsMemberClick} />
+                ) : (
+                    <SignIn handleIsMemberClick={handleIsMemberClick} />
+                )}
+            </div>
+            <div>Footer</div>
+        </>
+    );
+};
+
+export default Auth;
---
+++ client/src/pages/Auth/Signup.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Signup = ({ handleIsMemberClick }) => {
+    return (
+        <div className='flex flex-col bg-opacity-75 bg-black rounded-lg w-[420px] p-8 shadow-xl mx-auto my-10'>
+            <div className='mx-10'>
+                <h1 className='text-white text-5xl font-bold my-10'>Sign up</h1>
+                <input
+                    type='text'
+                    className='border-1 text-2xl border-black-500 rounded-lg bg-gray-600 h-50 px-4 my-3 py-4 w-full justify-center'
+                    placeholder='Email'
+                />
+                <input
+                    type='text'
+                    className='border-1 text-2xl border-black-500 rounded-lg bg-gray-600 h-50 px-4 my-3 py-4 w-full justify-center'
+                    placeholder='Name'
+                />
+                <input
+                    type='text'
+                    className='border-1 text-2xl border-black-500 rounded-lg bg-gray-600 h-50 px-4 my-3 py-4 w-full justify-center'
+                    placeholder='Password'
+                />
+                <input
+                    type='text'
+                    className='border-1 text-2xl border-black-500 rounded-lg bg-gray-600 h-50 px-4 my-3 py-4 w-full justify-center'
+                    placeholder='Confirm Password'
+                />
+                <button className='w-full text-white bg-green-700 rounded-lg my-9 self-center text-lg font-bold p-4'>
+                    Sign up
+                </button>
+                <h2 className='text-black-600 font-bold my-12 text-2xl'>
+                    Already a Member?{' '}
+                    <a
+                        className='inline text-2xl text-white underline'
+                        onClick={handleIsMemberClick}
+                    >
+                        Sign in.
+                    </a>
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
