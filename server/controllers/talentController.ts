740de20fb21092f737328ce6fd07de499a97ec99
Jojo Thomas <jojothomas1515@gmail.com>
2023-10-09 09:21:54 +0100

    added middleware for talent auth, and route to update delete and get the current talent/user

--- server/controllers/talentController.ts
+++ server/controllers/talentController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Talent from '../models/talentModel';
 import { CustomError } from '../errors/customError';
+import { Talent as TT } from '../interfaces/talentInterface';
 
 /**
  *  Returns all talents objects from the database and
@@ -13,8 +14,8 @@ export const allTalents = async (
     _req: Request,
     res: Response,
 ): Promise<Response> => {
-    const users = await Talent.find({});
-    return res.json(users);
+    const talents = await Talent.find({});
+    return res.json(talents);
 };
 
 /**
@@ -36,13 +37,89 @@ export const getTalentById = async (
             throw new CustomError('Required an id', 400);
         }
 
-        const user = await Talent.findById(id);
-        if (!user) {
-            throw new CustomError('Required an id', 404);
+        const talent = await Talent.findById(id);
+        if (!talent) {
+            throw new CustomError('Talent not found', 404);
         }
 
-        return res.json(user);
+        return res.json(talent);
     } catch (err) {
         next(err);
     }
+};
+
+/**
+ * Updates a talent's profile information.
+ * @async
+ * @function
+ * @param {Request & { talent: Talent }} req - The request object containing the talent to update.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<Response | undefined>} The updated talent object.
+ * @throws {Error} If an error occurs while updating the talent.
+ */
+export const updateTalent = async (
+    req: Request & { talent: Talent },
+    res: Response,
+    next: NextFunction,
+): Promise<Response | undefined> => {
+    try {
+        let { talent } = req;
+        const {
+            name,
+            bio,
+            profileImg,
+            projects,
+            certifications,
+            github,
+            socials,
+            skills,
+        }: TT = req.body;
+
+        talent = (await Talent.findOneAndUpdate(
+            { email: talent.email },
+            {
+                name,
+                bio,
+                profileImg,
+                projects,
+                certifications,
+                github,
+                socials,
+                skills,
+            },
+            { new: true },
+        )) as Talent;
+
+        return res.json(talent);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const deleteTalent = async (
+    req: Request & { talent: Talent },
+    res: Response,
+    next: NextFunction,
+): Promise<Response | undefined> => {
+    try {
+        const { talent } = req;
+
+        await Talent.findOneAndDelete({
+            email: talent.email,
+            name: talent.name,
+        });
+
+        return res.status(204).json();
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getCurrentTalent = async (
+    req: Request & { talent: Talent },
+    res: Response,
+): Promise<Response | undefined> => {
+    const { talent } = req;
+    return res.json(talent);
 };
--- server/routes/talentRoute.ts
+++ server/routes/talentRoute.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
-import { allTalents, getTalentById } from '../controllers/talentController';
+import {
+    allTalents,
+    deleteTalent,
+    getCurrentTalent,
+    getTalentById,
+    updateTalent,
+} from '../controllers/talentController';
+import { isTalentAuthenticated } from '../middlewares/talentAuth';
 
 export const talentRoutes = Router();
+// TODO: add talent auth middleware before these routes
+talentRoutes.use('/me', isTalentAuthenticated);
+
+talentRoutes.get('/me', getCurrentTalent);
+talentRoutes.put('/me', updateTalent);
+talentRoutes.delete('/me', deleteTalent);
 
 talentRoutes.get('/', allTalents);
 talentRoutes.get('/:id', getTalentById);
-
-// TODO: add talent auth middleware before these routes
