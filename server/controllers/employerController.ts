ee91550da0547f698cc550f46bf51b5c05dadbe8
Jojo Thomas <jojothomas1515@gmail.com>
2023-10-09 12:21:58 +0100

    employerSignup

--- server/controllers/employerController.ts
+++ server/controllers/employerController.ts
@@ -1,4 +1,7 @@
+import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
 import Employer from '../models/employerModel';
+import { CustomError } from '../errors/customError';
 
 export const createEmployer = async (req, res) => {
     const employerData = req.body;
@@ -11,5 +14,26 @@ export const createEmployer = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).send('Internal Server error');
+    }
+};
+
+export const signupEmployer = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<Response | undefined> => {
+    const { name, email, password } = req.body;
+
+    try {
+        const empExists = await Employer.findOne({ email });
+        if (empExists) return res.json({ message: 'user already exist' });
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const employer = await Employer.create({ name, email, hashedPassword });
+        if (!employer) throw new CustomError('Could not create user', 400);
+        await employer.save();
+        return res.status(201).json({ message: 'user created successfully' });
+    } catch (err) {
+        next(err);
     }
 };
