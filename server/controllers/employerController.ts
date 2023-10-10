21c596cc11427744005b0a1dbd2200682e5d3848
Jojo Thomas <jojothomas1515@gmail.com>
2023-10-10 22:11:34 +0100

    employer signup and login

--- server/controllers/employerController.ts
+++ server/controllers/employerController.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import Employer from '../models/employerModel';
 import { CustomError } from '../errors/customError';
+
+const JWT_SECRET = process.env.JWT_SECRET ?? 'your-secret-key';
 
 export const createEmployer = async (req, res) => {
     const employerData = req.body;
@@ -17,7 +20,7 @@ export const createEmployer = async (req, res) => {
     }
 };
 
-export const signupEmployer = async (
+export const employerSignup = async (
     req: Request,
     res: Response,
     next: NextFunction,
@@ -25,14 +28,61 @@ export const signupEmployer = async (
     const { name, email, password } = req.body;
 
     try {
+        if (!name) throw new CustomError('name field required', 400);
+        if (!email) throw new CustomError('email field required', 400);
+        if (!password) throw new CustomError('password field required', 400);
         const empExists = await Employer.findOne({ email });
         if (empExists) return res.json({ message: 'user already exist' });
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const employer = await Employer.create({ name, email, hashedPassword });
+        const employer = await Employer.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
         if (!employer) throw new CustomError('Could not create user', 400);
         await employer.save();
-        return res.status(201).json({ message: 'user created successfully' });
+
+        const accessToken = jwt.sign({ id: employer._id }, JWT_SECRET, {
+            expiresIn: '24h',
+        });
+        return res.status(201).json({
+            accessToken,
+            message: 'user created successfully',
+            data: {
+                ...employer.toJSON(),
+                password: undefined,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const employerLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<Response | undefined> => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email) throw new CustomError('email field required', 400);
+        if (!password) throw new CustomError('password field required', 400);
+
+        const employer = await Employer.findOne({ email });
+        if (!employer) throw new CustomError('User not found', 404);
+        const accessToken = jwt.sign({ id: employer._id }, JWT_SECRET, {
+            expiresIn: '24h',
+        });
+        return res.status(201).json({
+            accessToken,
+            message: 'user logged in successfully',
+            data: {
+                ...employer.toJSON(),
+                password: undefined,
+            },
+        });
     } catch (err) {
         next(err);
     }
--- server/routes/employerRoute.ts
+++ server/routes/employerRoute.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-import { createEmployer } from '../controllers/employerController';
+import {
+    createEmployer,
+    employerSignup,
+    employerLogin,
+} from '../controllers/employerController';
 
 const router = express.Router();
 
 router.post('/create', createEmployer);
+router.post('/signup', employerSignup);
+router.post('/login', employerLogin);
 
 export default router;
