a3a3eb3939118e858c5e5d741950e23e0e543795
Ayobami <ayobamidele006@gmail.com>
2023-10-12 17:17:52 +0100

    chore: clean staging branch

--- client/src/components/SectionTitle.tsx
+++ client/src/components/SectionTitle.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 interface SectionTitleProps {
     title: string;
     body: any;
@@ -5,11 +6,11 @@ interface SectionTitleProps {
 
 const SectionTitle: React.FC<SectionTitleProps> = ({ title, body }) => {
     return (
-        <div className="w-full md:w-1/2 p-4 mb-8 rounded bg-slate-900 text-white">
-            <h1 className="text-3xl font-semibold mb-4">{title}</h1>
+        <div className='w-full md:w-1/2 p-4 mb-8 rounded bg-slate-900 text-white'>
+            <h1 className='text-3xl font-semibold mb-4'>{title}</h1>
             <p>{body}</p>
         </div>
     );
-}
+};
 
 export default SectionTitle;
