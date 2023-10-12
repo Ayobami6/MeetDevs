2cef06a531a138a3e153b7e7261bbfc615e2f759
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-10-12 21:58:18 +0100

    feat: creaed Talent Card component and implemented dummy content for employer page

--- client/src/components/DatePicker.tsx
+++ client/src/components/DatePicker.tsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+// DatePickerComponent is a functional component that returns a DatePicker
 const DatePickerComponent: React.FC = () => {
+  // State for selected date
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
+  // Return DatePicker with necessary props
   return (
     <DatePicker
       selected={selectedDate}
