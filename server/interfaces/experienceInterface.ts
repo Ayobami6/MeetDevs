169f5728321005581480543621671821e20d737c
Ayobami <ayobamidele006@gmail.com>
2023-10-12 09:55:59 +0100

    feat: add seperate models for the following
    certification
    experience,
    skills,
    education and
    project

---
+++ server/interfaces/experienceInterface.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+    talentId: string;
+    jobTitle: string;
+    employer: string;
+    startDate: Date;
+    endDate: Date;
+    country: string;
+    description: string;
+}
---
+++ server/interfaces/project.ts
@@ -0,0 +1,8 @@
+export interface Project {
+    talentId: string;
+    name?: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+    link?: string;
+}
