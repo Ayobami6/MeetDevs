844ebefb0c31c7a4c3da8a5af01f38c5334dde35
Ayobami <ayobamidele006@gmail.com>
2023-10-13 07:13:12 +0100

    feat: add skills resource endpoints

--- server/controllers/skillController.ts
+++ server/controllers/skillController.ts
@@ -45,3 +45,35 @@ export const getASkill = async <T>(
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
+
+// update skill
+export const updateSkill = async <T>(
+    req: GenericRequest<T>,
+    res: GenericResponse<ResponseDocument, string>
+) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const skill = await Skill.findByIdAndUpdate(id, data);
+        if (!skill) return res.status(404).json({ message: 'Skill not found' });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+// delete skill
+export const deleteSkill = async <T>(
+    req: GenericRequest<T>,
+    res: GenericResponse<ResponseDocument, string>
+) => {
+    try {
+        const { id } = req.params;
+        const skill = await Skill.findByIdAndDelete(id);
+        if (!skill) return res.status(404).json({ message: 'Skill Not Found' });
+        res.status(200).json({ message: 'Skill deleted successfully' });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: 'Internal Server Error!' });
+    }
+};
