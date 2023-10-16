84884b9b6f2efa7aa90418c9f6135ba788ff1d14
betiniakarandut <betiniakarandut@gmail.com>
2023-10-16 21:27:32 +0100

    Merge conflict: Resolving

--- server/pagination/talentPagination.ts
+++ server/pagination/talentPagination.ts
@@ -8,14 +8,26 @@ import Talent from '../models/talentModel';
  * @param res json containing 12 talents, current page number
  * total number of pages and talents.
  */
+<<<<<<< HEAD
 const paginateTalents = async (req: Request, res: Response) => {
+=======
+const paginateTalents = async (
+    req: Request,
+    res: Response
+) => {
+>>>>>>> 3be3c97 (Feat: Talent pagination)
     const rawPage = req.query.page as string | undefined;
     const page = rawPage ? parseInt(rawPage) : 1; // Current page, default to 1
     const perPage = 12; // Number of talents per page
 
     try {
         const totalTalents = await Talent.countDocuments();
+<<<<<<< HEAD
         const talents = await Talent.find()
+=======
+        const talents = await Talent
+            .find()
+>>>>>>> 3be3c97 (Feat: Talent pagination)
             .skip((page - 1) * perPage)
             .limit(perPage);
 
@@ -26,7 +38,11 @@ const paginateTalents = async (req: Request, res: Response) => {
             talents,
         });
     } catch (error) {
+<<<<<<< HEAD
         errorHandler(error, res, req, error.NextFunction);
+=======
+        errorHandler(error, req, res, error.NextFunction);
+>>>>>>> 3be3c97 (Feat: Talent pagination)
     }
 };
 
