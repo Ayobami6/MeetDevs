cbe082e6a2720b18528f76f91b8c7b28e6558378
Ayobami Alaran A <ayobamidele006@gmail.com>
2023-10-16 08:37:59 +0100

    Employer user read/write 40% progress (#64)
    * feat: add x and linkedn socials to talent table
    
    * feat: add more page routes&adjust navbar
    
    Add talenthire, getranked, and employeroffer route pages
    Adjust navbar to detect user logged in
    
    * bug: fix assets images not loading in details page
    
    Changed img src to absolute path and move all asset files to public directory
    
    * feat: implement Show talent initial page
    
    Implemented minimal talent details page from the employers account consuming api to get one talent by id
    
    * fix: fix eslint useEffect dependencies

--- client/src/pages/Talent/ShowTalent.tsx
+++ client/src/pages/Talent/ShowTalent.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Loading from '../../components/Loading/Loading';
+import TalentNav from '../../components/Navbar/TalentNav';
+import { getTalent } from '../../actions/talent';
 
 const ShowTalent = () => {
-	return <div>ShowTalent</div>;
+	const talent = useSelector((state) => state.talents.talent);
+	const dispatch = useDispatch();
+	const [loading, setLoading] = useState();
+	const { id } = useParams();
+
+	useEffect(() => {
+		dispatch(getTalent(setLoading, id));
+	}, [id, dispatch]);
+	console.log(talent);
+
+	return (
+		<>
+			<TalentNav />
+			<div>
+				{loading ? (
+					<Loading />
+				) : (
+					<div>
+						<h1>{talent.name}</h1>
+						<h2>Id: {talent._id}</h2>
+					</div>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default ShowTalent;
