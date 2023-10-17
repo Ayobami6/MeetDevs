ee4dcfa4c27f8a35d8444f6856da672a1e18c053
Jojo Thomas <jojothomas1515@gmail.com>
2023-10-17 15:06:27 +0100

    cards complete

--- client/src/components/talentProfile/ShowwcaseCard.tsx
+++ client/src/components/talentProfile/ShowwcaseCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import TButton from "../Button/TButton.tsx";
 import "./ShowwcaseCard.css";
 import { noOfMonths } from "../util/numOfMonths.ts";
+import { FaGlobe } from "react-icons/fa";
 
 interface experienceProp {
   experience: {
@@ -26,6 +27,35 @@ interface projectProps {
   };
 }
 
+interface educationProps {
+  education: {
+    talentId: string;
+    degree: string;
+    school: string;
+    country: string;
+    city: string;
+    startDate: Date;
+    endDate: Date;
+    description?: string;
+  };
+}
+
+interface certificateProps {
+  certificate: {
+    talentId: string;
+    title?: string;
+    description?: string;
+    link?: string;
+  };
+}
+
+interface skillProps {
+  skill: {
+    talentId: string;
+    name?: string;
+    subSkills?: string;
+  };
+}
 const ExperienceShowwcaseCard: React.FC<experienceProp> = ({
   experience,
 }): React.ReactElement => {
@@ -61,12 +91,14 @@ const ProjectShowwcaseCard: React.FC<projectProps> = ({
 }): React.ReactElement => {
   return (
     <div className={"showwcasecard"}>
-      <div className={"experience"}>
+      <div className={"project"}>
         <div className={"title"}>
           <h1>{project.name}</h1>
         </div>
         <div className={"project-info"}>
-          <p>{project.link}</p>
+          <a href={project.link}>
+            <FaGlobe />
+          </a>
           &bull;
           <p>
             {project.startDate.getFullYear()} - {project.endDate.getFullYear()}
@@ -83,11 +115,71 @@ const ProjectShowwcaseCard: React.FC<projectProps> = ({
     </div>
   );
 };
-const EducationShowwcaseCard: React.FC = (): React.ReactElement => {
+const EducationShowwcaseCard: React.FC<educationProps> = ({
+  education,
+}): React.ReactElement => {
   return (
     <div className={"showwcasecard"}>
-      <div></div>
-      <div></div>
+      <div className={"experience"}>
+        <div className={"title"}>
+          <h1>{education.degree}</h1>
+        </div>
+        <div className={"work-info"}>
+          <p>{education.school}</p>
+          &bull;
+          <p>
+            {education.startDate.getFullYear()} -{" "}
+            {education.endDate.getFullYear()}
+          </p>
+          &bull;
+          <p>{noOfMonths(education.startDate, education.endDate)} mo</p>
+        </div>
+        <div className={"location-info"}>
+          <p>{education.country}</p>
+          <p>{education.city}</p>
+        </div>
+        <div className={"description"}>
+          <p>{education.description}</p>
+        </div>
+      </div>
+      <TButton value={"edit"} />
+    </div>
+  );
+};
+const certificationShowwcaseCard: React.FC<certificateProps> = ({
+  certificate,
+}): React.ReactElement => {
+  return (
+    <div className={"showwcasecard"}>
+      <div className={"certificate"}>
+        <div className={"title"}>
+          <h1>{certificate.title}</h1>
+        </div>
+        <div className={"work-info"}>
+          <a href={certificate.link}>View Certification</a>
+        </div>
+        <div className={"description"}>
+          <p>{certificate.description}</p>
+        </div>
+      </div>
+      <TButton value={"edit"} />
+    </div>
+  );
+};
+const skillShowwcaseCard: React.FC<skillProps> = ({
+  skill,
+}): React.ReactElement => {
+  return (
+    <div className={"showwcasecard"}>
+      <div className={"certificate"}>
+        <div className={"title"}>
+          <h1>{skill.name}</h1>
+        </div>
+        <div className={"work-info"}>
+          <p>{skill.subSkills}</p>
+        </div>
+      </div>
+      <TButton value={"edit"} />
     </div>
   );
 };
@@ -96,4 +188,6 @@ export {
   ExperienceShowwcaseCard,
   ProjectShowwcaseCard,
   EducationShowwcaseCard,
+  certificationShowwcaseCard,
+  skillShowwcaseCard,
 };
