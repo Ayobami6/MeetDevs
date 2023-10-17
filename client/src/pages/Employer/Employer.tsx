3b288be576f2ced9a137e214241b3b0909c89e9c
Ayobami <ayobamidele006@gmail.com>
2023-10-17 23:24:29 +0100

    fix: fix eslint error

--- client/src/pages/Employer/Employer.tsx
+++ client/src/pages/Employer/Employer.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import TalentCard from '../../components/TalentCard';
 import TalentNav from '../../components/Navbar/TalentNav';
 import Search from '../../components/util/Search';
@@ -8,44 +7,44 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getTalents } from '../../actions/talent';
 
 const Employer = () => {
-	const { talents } = useSelector((state) => state.talents);
-	const dispatch = useDispatch();
-	const [loading, setLoading] = useState();
+    const { talents } = useSelector((state) => state.talents);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState();
 
-	useEffect(() => {
-		dispatch(getTalents(setLoading));
-	}, [dispatch]);
-	console.log(talents);
-	return (
-		<>
-			<TalentNav />
+    useEffect(() => {
+        dispatch(getTalents(setLoading));
+    }, [dispatch]);
+    console.log(talents);
+    return (
+        <>
+            <TalentNav />
 
-			<div className='bg-gray-100 min-h-screen p-5'>
-				<div className='flex mx-10 justify-end'>
-					<Search />
-				</div>
-				<h1 className='text-3xl font-bold mb-5'>Available Talents</h1>
-				{!loading ? (
-					<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-						{talents.map((talent) => (
-							<TalentCard
-								name={talent.name}
-								key={talent._id}
-								jobRole={talent.jobRole}
-								location={talent.location}
-								bio={talent.bio}
-								github={talent.github}
-								profileImg={talent.profileImg}
-								id={talent._id}
-							/>
-						))}
-					</div>
-				) : (
-					<Loading />
-				)}
-			</div>
-		</>
-	);
+            <div className='bg-gray-100 min-h-screen p-5'>
+                <div className='flex mx-10 justify-end'>
+                    <Search />
+                </div>
+                <h1 className='text-3xl font-bold mb-5'>Available Talents</h1>
+                {!loading ? (
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                        {talents.map((talent) => (
+                            <TalentCard
+                                name={talent.name}
+                                key={talent._id}
+                                jobRole={talent.jobRole}
+                                location={talent.location}
+                                bio={talent.bio}
+                                github={talent.github}
+                                profileImg={talent.profileImg}
+                                id={talent._id}
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <Loading />
+                )}
+            </div>
+        </>
+    );
 };
 
 export default Employer;
