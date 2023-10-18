66aa1528ff9dd5ce7355c81e2b481992c9af809c
Ayobami <ayobamidele006@gmail.com>
2023-10-18 16:55:30 +0100

    bug: fix talentUser null not allowing page render

--- client/src/pages/Talent/GetRanked.tsx
+++ client/src/pages/Talent/GetRanked.tsx
@@ -9,7 +9,6 @@ const GetRanked = () => {
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
     const [notify, setNotify] = useState(false);
-    const [showRank, setShowRank] = useState(false);
     const { talent } = JSON.parse(localStorage.getItem('talentProfile'));
     const scores = useSelector((state) => state.score.talentScores);
     const talentUser = useSelector((state) => state.talents.talent);
@@ -25,7 +24,6 @@ const GetRanked = () => {
     useEffect(() => {
         dispatch(getTalent(setLoading, talentId));
         dispatch(getScoresTalent(setLoading));
-        setShowRank(true);
     }, [dispatch, talentId]);
 
     const calculatePercentile = async () => {
@@ -92,7 +90,7 @@ const GetRanked = () => {
     };
 
     console.log(scores);
-    console.log(talentUser);
+
     return (
         <div>
             <TalentNav />
@@ -103,43 +101,43 @@ const GetRanked = () => {
             {/* <div className='flex border-[1px] p-4 border-green-200 rounded-md w-[300px] m-10 justify-start text-2xl font-bold'>
                 Experimental Feature
             </div> */}
-            <div className='border-[1px] border-green-200 p-4 h-[100%] w-[90%] rounded-md mx-auto'>
-                <h1 className='text-center font-md text-2xl'>
-                    Get Ranked Experimental
-                </h1>
-                <h3 className='text-center text-md font-serif mt-5 mb-20'>
-                    This is an experimental feature that calculates rank
-                    percentiles base on your years of experience, skills, and
-                    collaborations
-                </h3>
-                <h3 className='border-[1px] py-4 rounded-md w-[60%] mx-auto text-center text-xl font-medium mb-5'>
-                    {!showRank
-                        ? ''
-                        : notify
-                        ? `Your New Percentile is ${talentUser.rank}%`
-                        : `Your Current Percentile is ${talentUser.rank}%`}
-                </h3>
-                <h4 className='text-center text-md '>
-                    Please before you click on test make sure to add your github
-                    social account
-                </h4>
-                <div className='flex justify-center mt-3'>
-                    {loading ? (
-                        <div className='w-10 h-10 border-t-2 border-green-500 border-solid rounded-full animate-spin'></div>
-                    ) : (
-                        ''
-                    )}
-                </div>
+            {talentUser && (
+                <div className='border-[1px] border-green-200 p-4 h-[100%] w-[90%] rounded-md mx-auto'>
+                    <h1 className='text-center font-md text-2xl'>
+                        Get Ranked Experimental
+                    </h1>
+                    <h3 className='text-center text-md font-serif mt-5 mb-20'>
+                        This is an experimental feature that calculates rank
+                        percentiles base on your years of experience, skills,
+                        and collaborations
+                    </h3>
+                    <h3 className='border-[1px] py-4 rounded-md w-[60%] mx-auto text-center text-xl font-medium mb-5'>
+                        {notify
+                            ? `Your New Percentile is ${talentUser.rank}%`
+                            : `Your Current Percentile is ${talentUser.rank}%`}
+                    </h3>
+                    <h4 className='text-center text-md '>
+                        Please before you click on test make sure to add your
+                        github social account
+                    </h4>
+                    <div className='flex justify-center mt-3'>
+                        {loading ? (
+                            <div className='w-10 h-10 border-t-2 border-green-500 border-solid rounded-full animate-spin'></div>
+                        ) : (
+                            ''
+                        )}
+                    </div>
 
-                <div className='flex justify-center m-10'>
-                    <button
-                        className='bg-green-500 animate-bounce text-xl hover:bg-green-700 rounded-xl w-[140px] h-[40px]'
-                        onClick={handleClick}
-                    >
-                        Test
-                    </button>
+                    <div className='flex justify-center m-10'>
+                        <button
+                            className='bg-green-500 animate-bounce text-xl hover:bg-green-700 rounded-xl w-[140px] h-[40px]'
+                            onClick={handleClick}
+                        >
+                            Test
+                        </button>
+                    </div>
                 </div>
-            </div>{' '}
+            )}
         </div>
     );
 };
