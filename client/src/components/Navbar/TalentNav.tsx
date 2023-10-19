4187647d80bb9aff7e67b1da4be18955edf93e6e
betiniakarandut <betiniakarandut@gmail.com>
2023-10-19 04:09:34 +0100

    Conflict merge: Resolved

--- client/src/components/Navbar/TalentNav.tsx
+++ client/src/components/Navbar/TalentNav.tsx
@@ -1,25 +1,36 @@
-import { Link } from "react-router-dom";
-import "./TalentNav.css";
+import { Link, useNavigate } from 'react-router-dom';
+import './TalentNav.css';
 
 function TalentNav(): JSX.Element {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate('/auth');
+  };
   return (
-    <div className="talent-nav">
-      <div className="nav-con">
-        <img className={"rounded-full"} src="src/assets/logo.png" alt="" />
-        <nav className="nav-items-con">
-          <Link to={"#"}>Get Ranked</Link>
-          <Link to={"#"}>Messages</Link>
-          <Link to={"#"}>Offers</Link>
+    <div className='talent-nav'>
+      <div className='nav-con'>
+        <img className={'rounded-full'} src='src/assets/logo.png' alt='' />
+        <nav className='nav-items-con'>
+          <Link to={'#'}>Get Ranked</Link>
+          <Link to={'#'}>Messages</Link>
+          <Link to={'#'}>Offers</Link>
         </nav>
       </div>
 
-      <Link to={"#"}>
+      <Link to={'#'}>
         <img
-          src="src/assets/talents/no_image.png"
-          alt="fine girl"
-          className="w-20 rounded-full"
+          src='src/assets/talents/no_image.png'
+          alt='fine girl'
+          className='w-20 rounded-full'
         />
       </Link>
+      <button
+        className='border-white-300 bg-green-600 hover:bg-green-300 rounded-lg border-2 w-36 h-10'
+        onClick={logout}
+      >
+        Log out
+      </button>
     </div>
   );
 }
---
+++ client/src/components/util/ProtectedRoute.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = ({ profileObj }) => {
+  const profile = JSON.parse(localStorage.getItem(profileObj));
+
+  return profile ? <Outlet /> : <Navigate to='/auth' />;
+};
+
+export default ProtectedRoute;
