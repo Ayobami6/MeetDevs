0a9c308c06adfa5345525bfa71bf61c779cc83c4
Ayobami <ayobamidele006@gmail.com>
2023-10-19 09:24:23 +0100

    fix: fix talentMeta reducer to have seperate data
    Fix talentMeta reducer to have seperate state data objects for experience, education, project, certifications and skills

--- client/src/reducers/talentMeta.ts
+++ client/src/reducers/talentMeta.ts
@@ -1,18 +1,62 @@
 export const metaReducer = (
     state = {
-        talentMetas: [],
-        talentMeta: null,
+        certifications: [],
+        certification: null,
+        educations: [],
+        education: null,
+        experiences: [],
+        experience: null,
+        projects: [],
+        project: null,
+        skills: [],
+        skill: null,
     },
     action
 ) => {
     switch (action.type) {
-        case 'FETCH_TALENT_META':
-            return { ...state, talentMetas: action.payload };
-        case 'UPDATE_TALENT_META':
-        case 'CREATE_TALENT_META':
-            return { ...state, talentMeta: action.payload };
+        case 'FETCH_TALENT_CERTIFICATION':
+            return { ...state, certifications: action.payload };
+        case 'UPDATE_TALENT_CERTIFICATION':
+        case 'CREATE_TALENT_CERTIFICATION':
+            return { ...state, certification: action.payload };
 
-        case 'DELETE_TALENT_META':
+        case 'DELETE_TALENT_CERTIFICATION':
+            return { ...state };
+
+        case 'FETCH_TALENT_EDUCATION':
+            return { ...state, educations: action.payload };
+        case 'UPDATE_TALENT_EDUCATION':
+        case 'CREATE_TALENT_EDUCATION':
+            return { ...state, education: action.payload };
+
+        case 'DELETE_TALENT_EDUCATION':
+            return { ...state };
+
+        case 'FETCH_TALENT_EXPERIENCE':
+            return { ...state, experiences: action.payload };
+        case 'UPDATE_TALENT_EXPERIENCE':
+        case 'CREATE_TALENT_EXPERIENCE':
+            return { ...state, experience: action.payload };
+
+        case 'DELETE_TALENT_EXPERIENCE':
+            return { ...state };
+
+        case 'FETCH_TALENT_PROJECT':
+            return { ...state, projects: action.payload };
+        case 'UPDATE_TALENT_PROJECT':
+        case 'CREATE_TALENT_PROJECT':
+            return { ...state, project: action.payload };
+
+        case 'DELETE_TALENT_PROJECT':
+            return { ...state };
+
+        case 'FETCH_TALENT_SKILL':
+            return { ...state, skills: action.payload };
+        case 'UPDATE_TALENT_SKILL':
+        case 'CREATE_TALENT_SKILL':
+            return { ...state, skill: action.payload };
+
+        case 'DELETE_TALENT_SKILL':
             return { ...state };
 
         default:
