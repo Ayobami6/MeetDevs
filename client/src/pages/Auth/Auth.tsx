74b16eafd339e57379f02e1c59d18fbd277c7483
Ayobami <ayobamidele006@gmail.com>
2023-10-28 13:11:03 +0100

    fix: fix images not rendered on production

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -4,34 +4,31 @@ import SignUp from './Signup';
 import Navbar from '../../components/Navbar/Navbar2';
 
 const Auth: React.FC = () => {
-    const [isSignup, setIsSignup] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
 
-    const handleIsMemberClick = () => {
-        setIsSignup(!isSignup);
-    };
+  const handleIsMemberClick = () => {
+    setIsSignup(!isSignup);
+  };
 
-    return (
-        <>
-            <div>
-                {/* <div className="bg-[url('src/assets/auth3.jpg')] bg-img w-full bg-cover">
+  return (
+    <>
+      <div>
+        {/* <div className="bg-[url('src/assets/auth3.jpg')] bg-img w-full bg-cover">
                     
                 </div> */}
-                <img
-                    src='src/assets/auth3.jpg'
-                    className='bg-img bg-cover w-fit'
-                />
-                <div className='text-left mx-3 py-4'>
-                    <Navbar isAuth={true} />
-                </div>
-                {isSignup ? (
-                    <SignUp handleIsMemberClick={handleIsMemberClick} />
-                ) : (
-                    <SignIn handleIsMemberClick={handleIsMemberClick} />
-                )}
-            </div>
-            <div>Footer</div>
-        </>
-    );
+        <img src='assets/auth3.jpg' className='bg-img bg-cover w-fit' />
+        <div className='text-left mx-3 py-4'>
+          <Navbar isAuth={true} />
+        </div>
+        {isSignup ? (
+          <SignUp handleIsMemberClick={handleIsMemberClick} />
+        ) : (
+          <SignIn handleIsMemberClick={handleIsMemberClick} />
+        )}
+      </div>
+      <div>Footer</div>
+    </>
+  );
 };
 
 export default Auth;
